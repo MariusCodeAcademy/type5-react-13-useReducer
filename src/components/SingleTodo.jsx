152f6dba3b217984e2ccc1096958c1a0a0b3652a
Marius <marius@example.com>
2022-04-12 17:00:08 +0300

    checkTodo

---
+++ src/components/SingleTodo.jsx
@@ -0,0 +1,15 @@
+// SingleTodo grazinti li kurio title gauna is props
+import css from './SingleTodo.module.css';
+
+function SingleTodo(props) {
+  return (
+    <li style={{ textDecoration: props.isDone ? 'line-through' : 'none' }}>
+      <span>{props.title}</span>
+      <button onClick={() => props.onCheck(props.id)} className={css.btn}>
+        check
+      </button>
+    </li>
+  );
+}
+
+export default SingleTodo;
--- src/components/TodoListReducer.jsx
+++ src/components/TodoListReducer.jsx
@@ -1,10 +1,11 @@
 // sukurti komponenta
 import { v4 as uuid } from 'uuid';
 import { useReducer, useState } from 'react';
+import SingleTodo from './SingleTodo';
 // turi antraste
 // turi saraso el su li
 const initTodos = [
-  { id: 1, title: 'Go to park', doneStatus: false },
+  { id: 1, title: 'Go to park', doneStatus: true },
   { id: 2, title: 'Go to mall', doneStatus: false },
 ];
 
@@ -12,7 +13,13 @@ function todoReducer(todos, action) {
   console.log('action ===', action);
   switch (action?.type) {
     case 'addTodo':
-      return todos.concat(action.payload);
+      return [...todos, action.payload];
+    case 'checkTodo':
+      const todoCopy = [...todos];
+      const found = todoCopy.find((tObj) => tObj.id === action.payload);
+      console.log('found ===', found);
+      found.doneStatus = !found.doneStatus;
+      return todoCopy;
     default:
       throw new Error('todoReducer.No action type found');
   }
@@ -24,14 +31,19 @@ function TodoListReducer() {
   const [todoTitle, setTodoTitle] = useState('Do 100 pushups');
   // sujungti todoTitle state su  input el
 
+  function checkHandler(todoId) {
+    dispatch({ type: 'checkTodo', payload: todoId });
+  }
+
   function submitHandler(e) {
     e.preventDefault();
-    console.log('react is in controll');
+    if (!todoTitle.length) return;
     // sukurti naujo todo obj
     //{ id: 2, title: 'Go to mall', doneStatus: false }
     const newTodoObj = { id: uuid(), title: todoTitle, doneStatus: false };
     // prideti prie esamu
     dispatch({ type: 'addTodo', payload: newTodoObj });
+    setTodoTitle('');
   }
   return (
     <div className='counter'>
@@ -47,7 +59,13 @@ function TodoListReducer() {
       </form>
       <ul>
         {todos.map((tObj) => (
-          <li key={tObj.id}>{tObj.title}</li>
+          <SingleTodo
+            onCheck={checkHandler}
+            isDone={tObj.doneStatus}
+            title={tObj.title}
+            id={tObj.id}
+            key={tObj.id}
+          />
         ))}
       </ul>
     </div>
