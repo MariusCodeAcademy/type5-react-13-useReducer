c97990cafe708ac995c9bae8548caba94c98a6e3
Marius <marius@example.com>
2022-04-12 14:20:08 +0300

    +5 count

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -8,20 +8,26 @@ import { useState } from 'react';
 function Counter() {
   const [count, setCount] = useState(0);
 
+  function handleIncrementBy(x) {
+    setCount((prevState) => prevState + x);
+  }
+
   function handleIncrement() {
-    console.log('+');
+    // console.log('+');
     // kei state priklauso nuo pries tai state tai atnaujinam su funkcija
     setCount((prevState) => prevState + 1);
     // setCount(count + 1); // no no
   }
   function handleDecrement() {
-    console.log('-');
+    // console.log('-');
+    setCount((prevState) => prevState - 1);
   }
 
   return (
     <div className='counter'>
       <h2>{count}</h2>
       <div className='control'>
+        <button onClick={() => handleIncrementBy(5)}>+5</button>
         <button onClick={handleIncrement}>+</button>
         <button onClick={handleDecrement}>-</button>
       </div>
